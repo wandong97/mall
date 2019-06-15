c183c7e792e57243fa64fd4f92c5817c34319b9c
Administrator <77777@qq.com>
2019-06-15 21:01:13 +0800

    登陆ok

--- Mall/src/main/java/com/wandong/dao/ProductDao.java
+++ Mall/src/main/java/com/wandong/dao/ProductDao.java
@@ -1,16 +1,13 @@
 package com.wandong.dao;
 
 import com.wandong.domin.Product;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
 @Repository
-public interface AccountDao {
-
-    // 查询所有 Account
-    @Select("Select * from product")
-    List<Product> findAll();
-
+public interface ProductDao {
+    @Select("select * from product where cid=#{cid}")
+    List<Product> findAll(String cid);
 }
